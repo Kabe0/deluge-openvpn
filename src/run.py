2c77b923f18f0523cc4ee3a1984a501a4a7ac107
Ian Carson <ian@bizzone.com>
2019-12-30 12:02:13 -0500

    Pushing in fixes for user ID

--- src/run.py
+++ src/run.py
@@ -4,6 +4,14 @@ import os
 if not os.path.exists("~/.config/deluge"):
     os.makedirs("~/.config/deluge")
 
+DEL_UID = os.getenv("DEL_UID", 1000)
+DEL_GID = os.getenv("DEL_GID", 1000)
+
+if DEL_UID != 1000:
+    subprocess.run(["/usr/sbin/usermod", "-u", f"{DEL_UID}", "deluge"])
+if DEL_GID != 1000:
+    subprocess.run(["/usr/sbin/groupmod", "-g", f"{DEL_GID}", "deluge"])
+
 webCmd = ["su", "deluge", "-c", "/usr/bin/deluge-web"]
 webPort = os.getenv("WEB_PORT", None)
 
